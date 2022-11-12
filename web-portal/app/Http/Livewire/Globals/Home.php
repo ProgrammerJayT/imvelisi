b94afb3b3243fb400cda979e99c6cd388443c8c1
Programmer JayT <theanthem8@gmail.com>
2022-11-12 17:52:30 +0200

    Created home component

---
+++ web-portal/app/Http/Livewire/Globals/Home.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Globals;
+
+use Livewire\Component;
+
+class Home extends Component
+{
+    public function render()
+    {
+        return view('livewire.globals.home');
+    }
+}
